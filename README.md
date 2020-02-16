91f9e9c471bb5d95a9af4b8beb9888b2f88a6c13
Michael Ponti <michael@brainbucketsoftware.com>
2020-02-16 11:27:02 -0800

    task: initial commit, setup with prism and mahapps

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # WebStorageUtil
 Helper utility for managing static websites in Azure storage accounts
+
+
+https://stackoverflow.com/questions/10040403/set-content-type-of-media-files-stored-on-blob
---
+++ StorageUtil/StorageUtil/App.xaml.cs
@@ -0,0 +1,37 @@
+﻿using Prism.Ioc;
+using Prism.Unity;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using Unity;
+
+
+namespace StorageUtil
+{
+	/// <summary>
+	/// Interaction logic for App.xaml
+	/// </summary>
+	public partial class App : PrismApplication
+	{
+		public App()
+		{
+
+		}
+
+
+		protected override Window CreateShell()
+		{
+			var w = Container.Resolve<MainWindow>();
+			return w;
+		}
+
+
+		protected override void RegisterTypes(IContainerRegistry containerRegistry)
+		{
+		}
+	}
+}
