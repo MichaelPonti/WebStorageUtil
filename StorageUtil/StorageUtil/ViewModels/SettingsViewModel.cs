d3844c36b00f095ef6d68a26e7fe9e498f897d37
Michael Ponti <michael@brainbucketsoftware.com>
2020-02-17 07:21:22 -0800

    task: add in view model

---
+++ StorageUtil/StorageUtil/ViewModels/SettingsViewModel.cs
@@ -0,0 +1,21 @@
+﻿using Prism.Logging;
+using StorageUtil.Services.Settings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StorageUtil.ViewModels
+{
+	public class SettingsViewModel : BaseViewModel
+	{
+		private ISettingsService _settingsService = null;
+
+		public SettingsViewModel(ILoggerFacade logService, ISettingsService settingsService)
+			: base(logService)
+		{
+			_settingsService = settingsService;
+		}
+	}
+}
